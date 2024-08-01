e036b9a2f2f980bf8142c1042b3566c56bc8bed2
Dillon Troxell <Dillon.Troxell@8451.com>
2024-08-01 16:41:48 -0400

    Worked on lexer logic. Not done yet.

--- Lexer.hpp
+++ Lexer.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include "token.hpp"
 #include "tokentype.hpp"
@@ -9,23 +10,53 @@ private:
   std::string fileContents;
   std::vector<Token> tokens;
   int cur_index;
-  char cur_char;
-  int length;
+  int contLength;
 public:
   // Constructor
   Lexer(std::string fileContents) {
     this->fileContents = fileContents;
-    this->length = fileContents.length();
-    this->cur
+    this->contLength = fileContents.length();
+    this->cur_index = 0;
   }
 
-  std::vector<Token> getTokens() {
+  bool isAtEnd(void) {
+    return this->cur_index >= this->contLength;
+  }
+
+  char peek(void) {
+    if(isAtEnd()) {
+      return '\0';
+    }
+    if(this->cur_index + 1 < this->contLength) {
+      this->fileContents.at(this->cur_index);
+    }
+    // Not another character left
+    return '\0'; 
+  }
+
+  char consume(void) {
+    if(isAtEnd()) {
+      return '\0'; 
+    }
+    return this->fileContents.at(this->cur_index++);
+  }
+
+  std::vector<Token> getTokens(void) {
     return this->tokens;
+  }
+  
+  void makeTokens(void) {
+    char curChar = consume();
+    if(curChar == '\0') {
+      throw std::runtime_error("Empty buffer");
+    }
+    while(!isAtEnd()) {
+      break;
+    }
   }
 
   void addToken(TokenType type, std::any data) {
     Token tok = Token(type, data);
     this->tokens.push_back(tok);
   } 
-
 };
